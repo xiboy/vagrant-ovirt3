f9a1d96770cb415da20290eb8c588b4627acf7c0
lionel <lionel@px-beard.pc.cls.fr>
2016-05-11 10:58:16 +0200

    Raise VM name limit from 15 to 80

--- lib/vagrant-ovirt3/action/create_vm.rb
+++ lib/vagrant-ovirt3/action/create_vm.rb
@@ -17,7 +17,7 @@ module VagrantPlugins
           config = env[:machine].provider_config
 
           # Gather some info about domain
-          name = (config.name.nil? ? env[:domain_name] : config.name)[0,15]
+          name = (config.name.nil? ? env[:domain_name] : config.name)[0,80]
           console = config.console
           cpus = config.cpus
           memory_guaranteed_size = config.memory_guaranteed ? config.memory_guaranteed*1024 : nil
