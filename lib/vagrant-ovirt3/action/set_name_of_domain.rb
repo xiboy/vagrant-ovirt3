fa1ca06cd7df3ed778cbfc129a2783506fccd6a5
xiboy <xiboy@users.noreply.github.com>
2016-05-12 11:50:24 +0200

    To bypass an oVirt 3.6.5 bug with hostname
    oVirt is unable to start a VM if its hostname contains -X_

--- lib/vagrant-ovirt3/action/set_name_of_domain.rb
+++ lib/vagrant-ovirt3/action/set_name_of_domain.rb
@@ -11,7 +11,7 @@ module VagrantPlugins
 
         def call(env)
           dir_name = env[:root_path].basename.to_s.dup.gsub(/[^-a-z0-9_]/i, "")
-          timestamp = "_#{Time.now.to_f}"
+          timestamp = "-#{Time.now.to_f}"
           max_dir_name_length = dir_name.length-[(dir_name + timestamp).length-(@@MAX_NAME_LENGTH-1), 0].max
           env[:domain_name] = dir_name[0..max_dir_name_length] << timestamp
 
